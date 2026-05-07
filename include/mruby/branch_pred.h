2f95230e19716f5393b3c1a7c1974f2dada33be7
Asmod4n <Asmod4n@users.noreply.github.com>
2026-05-07 21:10:39 +0200

    try to fix it for windows

--- include/mruby/branch_pred.h
+++ include/mruby/branch_pred.h
@@ -1,8 +1,15 @@
 #pragma once
-#if ((defined(__has_builtin) && __has_builtin(__builtin_expect))||(__GNUC__ >= 3) || (__INTEL_COMPILER >= 800) || defined(__clang__))
-#define likely(x)   __builtin_expect(!!(x), 1)
-#define unlikely(x) __builtin_expect(!!(x), 0)
+
+#ifndef __has_builtin
+#define __has_builtin(x) 0
+#endif
+
+#if __has_builtin(__builtin_expect) \
+    || (defined(__GNUC__) && __GNUC__ >= 3) \
+    || (defined(__INTEL_COMPILER) && __INTEL_COMPILER >= 800)
+#  define likely(x)   __builtin_expect(!!(x), 1)
+#  define unlikely(x) __builtin_expect(!!(x), 0)
 #else
-#define likely(x)   (x)
-#define unlikely(x) (x)
+#  define likely(x)   (x)
+#  define unlikely(x) (x)
 #endif
